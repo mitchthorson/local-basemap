fe45502eaa945e676218c12a9420c6f46e7fcf99
mitchthorson <mitchell@mitchellthorson.com>
2023-11-14 01:19:17 -0500

    Add NYC income data to map

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import maplibregl from "maplibre-gl"; // or "const maplibregl = require('maplibre-gl');"
-import layers from "protomaps-themes-base";
 import "./style.css";
 import "../node_modules/maplibre-gl/dist/maplibre-gl.css";
+import { interpolateInferno } from "d3-scale-chromatic";
+import { extent } from "d3-array";
+import { scaleSequential } from "d3-scale";
+
+import maplibregl from "maplibre-gl"; // or "const maplibregl = require("maplibre-gl");"
+import layers from "protomaps-themes-base";
+
 
 const mapStyle = {
   version: 8,
@@ -21,8 +26,37 @@ const mapStyle = {
 const map = new maplibregl.Map({
   container: "map",
   style: mapStyle, // stylesheet location
-	vounds: [[-74.389801,40.464711],[-73.218384,41.116607]],
-	maxBounds: [[-74.389801,40.464711],[-73.218384,41.116607]],
+  vounds: [
+    [-74.389801, 40.464711],
+    [-73.218384, 41.116607],
+  ],
+  maxBounds: [
+    [-74.389801, 40.464711],
+    [-73.218384, 41.116607],
+  ],
 });
 
 map.addControl(new maplibregl.NavigationControl());
+
+map.on("load", () => {
+  fetch("nyc_income.json")
+    .then((response) => response.json())
+    .then((data) => {
+			const color = scaleSequential(extent(data.features, d => d.properties.estimate ), interpolateInferno);
+			data.features = data.features.filter((d) => d.properties.estimate != null).map(d => {
+				d.properties.color = color(d.properties.estimate);
+				return d;
+			});
+      map.addSource("income", {
+        type: "geojson",
+        data: data,
+      });
+      map.addLayer({
+        id: "nyc_income",
+        type: "fill",
+        source: "income",
+        layout: {},
+        paint: { "fill-color": ["get", "color"], "fill-opacity": 0.75, },
+      });
+    });
+});
